0a22a4a4474f3b18f279bf124f599dfd069d1abf
Rina Rasia <rinarasia@gmail.com>
2023-10-19 21:51:34 -0700

    Rework CSS to rely on grid layout and auto-fit books on bookshelf

--- script.js
+++ script.js
@@ -11,10 +11,22 @@ function Book(title, author, pages, read) {
 
 }
 
-function addBookToLibrary() {
-  myLibrary.push();
+function addBookToLibrary(book) {
+  myLibrary.push(book);
 }
 
 const Tolkien = new Book('Tolkien', 'J.R.R.', '1342', true);
 
 console.log(Tolkien.info());
+
+addBookToLibrary(Tolkien);
+
+
+const tik = new Book('Tik', 'J.R.R.', '1342', true);
+const tok = new Book('Tok', 'J.R.R.', '1342', true);
+const ohh = new Book('Ohh', 'J.R.R.', '1342', true);
+
+addBookToLibrary(tik);
+addBookToLibrary(tok);
+addBookToLibrary(ohh);
+console.log(myLibrary[0].title);
