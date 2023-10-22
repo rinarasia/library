0f3c6d98532a165f8edd5a669ced810a1a7585d6
Rina Rasia <rinarasia@gmail.com>
2023-10-22 12:26:22 -0700

    Add script to display new book on bookshelf on form submit

--- script.js
+++ script.js
@@ -1,3 +1,80 @@
+window.onload = function () {
+  const $title = document.querySelector('#title');
+  const $author = document.querySelector('#author');
+  const $pages = document.querySelector('#pages');
+  const $read = document.querySelector('#read');
+
+  const bookshelf = document.getElementById('bookshelf');
+
+
+  const $form = document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // save new book
+    const addedBook = new Book($title.value, $author.value, $pages.value, $read.checked);
+
+    const bookTitle = $title.value;
+    const bookAuthor = $author.value;
+    const bookPages = $pages.value;
+
+    // add book to library
+    addBookToLibrary(addedBook);
+
+    // show new book on bookshelf
+
+    myLibrary.forEach(book => {
+      console.log(book.title);
+
+      // create new book
+      const newBook = document.createElement("div");
+      newBook.classList.add('book');
+
+      // Add title to book-----------------------------------
+      // create h4 inside book
+      const h4 = document.createElement("h4");
+      h4.classList.add('title');
+      newBook.appendChild(h4);
+
+      // create text for book
+      const newBookTitle = document.createTextNode(bookTitle);
+
+      // add title to book
+      h4.appendChild(newBookTitle);
+
+      // Add author to book ---------------------------------
+      // create span author inside book
+      const spanAuthor = document.createElement("span");
+      spanAuthor.classList.add('author');
+      newBook.appendChild(spanAuthor);
+
+      // create author text for book
+      const newBookAuthor = document.createTextNode(bookAuthor);
+
+      // add author to book
+      spanAuthor.appendChild(newBookAuthor);
+
+      // Add pages to book ---------------------------------
+      // create span pages inside book
+      const spanPages = document.createElement("span");
+      spanPages.classList.add('pages');
+      newBook.appendChild(spanPages);
+
+      // create author text for book
+      const newBookPages = document.createTextNode(bookPages + `pgs`);
+
+      // add author to book
+      spanPages.appendChild(newBookPages);
+
+      // add book to bookshelf
+      bookshelf.append(newBook);
+
+    });
+
+  });
+
+
+}
+
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -5,7 +82,7 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  this.info = function() {
+  this.info = function () {
     return title + ' by ' + author + ', ' + pages + ' Pages' + read;
   }
 
@@ -14,7 +91,7 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(book) {
   myLibrary.push(book);
 }
-
+/*
 const Tolkien = new Book('Tolkien', 'J.R.R.', '1342', true);
 
 console.log(Tolkien.info());
@@ -30,3 +107,20 @@ addBookToLibrary(tik);
 addBookToLibrary(tok);
 addBookToLibrary(ohh);
 console.log(myLibrary[0].title);
+*/
+
+
+
+/*
+
+    
+      // get bookshelf
+    
+      // create new book
+      const newBook = document.createElement("div");
+      newBook.classList.add('book');
+    
+      // add book to bookshelf
+      bookshelf.append(newBook);
+
+      */
